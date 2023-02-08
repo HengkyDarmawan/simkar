990e29bb1303c7d3851993de26d48c49b5c7c9e4
HengkyDarmawan <hengkydarmawan66@gmail.com>
2023-02-09 05:01:45 +0700

    tambah design template

--- application/views/absen/index.php
+++ application/views/absen/index.php
@@ -3,6 +3,28 @@
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <div class="d-flex">
+                <h6 class="m-0 font-weight-bold text-primary mr-auto p-2">Upload dan Donwload Data Absen</h6>
+                <button type="submit" class="btn btn-success"><i class="fas fa-download"></i> Donwload</button>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" width="100%" cellspacing="0">
+                    <tbody class="text-center">
+                    <div class="form-group">
+                        <label><strong>Data Absen</strong></label>
+                        <input type="file" name="status_pernikahan" class="form-control mb-4"> 
+                        <button type="submit" class="btn btn-primary"><i class="fas fa-upload"></i> Upload</button>
+                    </div>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
     <?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>') ?>
     <?= $this->session->flashdata('message'); ?>
 
--- application/views/data/pegawai.php
+++ application/views/data/pegawai.php
@@ -1,8 +1,30 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
-
+    
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <div class="d-flex">
+                <h6 class="m-0 font-weight-bold text-primary mr-auto p-2">Upload dan Donwload Data Pegawai</h6>
+                <button type="submit" class="btn btn-success"><i class="fas fa-download"></i> Donwload</button>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" width="100%" cellspacing="0">
+                    <tbody class="text-center">
+                    <div class="form-group">
+                        <label><strong>Data Pegawai</strong></label>
+                        <input type="file" name="status_pernikahan" class="form-control mb-4"> 
+                        <button type="submit" class="btn btn-primary"><i class="fas fa-upload"></i> Upload</button>
+                    </div>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
     <?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>') ?>
     <?= $this->session->flashdata('message'); ?>
 
